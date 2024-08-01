97105c3c42e4cfcca85868e208e89c34c4d4fbc0
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-01 19:09:03 +0200

    add assignment 1 and solution

--- euler_1/src/main.rs
+++ euler_1/src/main.rs
@@ -1,3 +1,26 @@
+const MAX_NUM: u128 = 1_000;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut sum_of_numbers: u128 = 0;
+    let mut i: u128 = 1;
+
+    loop {
+
+        let multiple_three = 3 * i;
+        let multiple_five = 5 * i;
+
+        if multiple_three < MAX_NUM {
+            sum_of_numbers += multiple_three;
+            if multiple_five < MAX_NUM && multiple_five % 3 != 0 {
+                sum_of_numbers += multiple_five;
+            }
+        } else {
+            break
+        }
+
+        i += 1;
+    }
+
+    println!("Sum of all multiples of 3 and 5 below {MAX_NUM} is {sum_of_numbers}.");
 }
