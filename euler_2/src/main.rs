4c710923675d71fd728f5bdc717052e5c5e61712
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-02 21:17:06 +0200

    assignment 2 done

--- euler_2/src/main.rs
+++ euler_2/src/main.rs
@@ -1,3 +1,23 @@
+const MAX_NUMBER_TO_CONSIDER: u64 = 4_000_000;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut first = 1;
+    let mut second = 2;
+    let mut new_term;
+    let mut sum = 0;
+
+    while second <= MAX_NUMBER_TO_CONSIDER {
+
+        if second % 2 == 0 {
+            sum += second;
+        }
+
+        new_term = first + second;
+        first = second;
+        second = new_term;
+
+    }
+    
+    println!("Sum of all even-valued Fibonacci numbers below {MAX_NUMBER_TO_CONSIDER} is {sum}");
 }
