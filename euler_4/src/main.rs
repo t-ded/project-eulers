501db4ad0aa20e50127b13cb1d90d54c6ce71ef9
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-03 21:01:51 +0200

    assignment 4 done

--- euler_4/src/main.rs
+++ euler_4/src/main.rs
@@ -1,3 +1,25 @@
+use String;
+
+fn is_palindromic(x: u64) -> bool {
+    let x_str = x.to_string();
+    x_str == x_str.chars().rev().collect::<String>()
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut max_palindrome = 0;
+
+    'outer: for left in (100..=999).rev() {
+        for right in (100..=left).rev() {
+            let prod = left * right;
+            if prod < max_palindrome {
+                continue 'outer;
+            } else if is_palindromic(prod) {
+                max_palindrome = prod;
+                continue 'outer;
+            }
+        }
+    }
+
+    println!("Largest palindrome product of 3-digit numbers is {max_palindrome}!");
 }
