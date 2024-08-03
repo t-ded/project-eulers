163370af72f9e916d355afd921d35a9fb20b7450
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-03 21:34:36 +0200

    assignment 5 done

--- euler_5/src/main.rs
+++ euler_5/src/main.rs
@@ -1,3 +1,23 @@
+const NEED_TO_DIVIDE: [u32; 10] = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11];
+
+fn is_divisible(x: u32) -> bool {
+    for num in NEED_TO_DIVIDE {
+        if x % num != 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut num = 40;
+
+    loop {
+        if is_divisible(num) {
+            println!("Smallest number divisible by numbers 1-20 is {num}");
+            break;
+        }
+        num += 20;
+    }
 }
