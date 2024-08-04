f7ae7857591fe2bd6a1fe28605a85c4f11d6573d
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-04 18:18:54 +0200

    assignment 9 done

--- euler_9/src/main.rs
+++ euler_9/src/main.rs
@@ -1,3 +1,13 @@
+const SUM: u32 = 1_000;
+
 fn main() {
-    println!("Hello, world!");
+    for a in 1..=(SUM / 3 - 1) {
+        for b in (a + 1)..=(SUM / 2 - a / 2 - 1) {
+            let c = SUM - b - a;
+            if a.pow(2) + b.pow(2) == c.pow(2) {
+                println!("The triplet is {a}, {b}, {c} with their product being {}.", a * b * c);
+                return;
+            }
+        }
+    }
 }
