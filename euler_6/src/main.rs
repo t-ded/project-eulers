cd79fda9325205f4b9f4a54efe3fa38b45674a56
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-04 11:54:53 +0200

    assignment 6 done

--- euler_6/src/main.rs
+++ euler_6/src/main.rs
@@ -1,3 +1,27 @@
+const MAX_NUMBER: usize = 100;
+
+fn sum_of_squares(numbers: &Vec<usize>) -> usize {
+    let mut sum = 0;
+    for num in numbers {
+        sum += num.pow(2);
+    }
+    return sum;
+}
+
+fn square_of_sum(numbers: &Vec<usize>) -> usize {
+    let sum: usize = numbers.iter().sum();
+    return sum.pow(2);
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let numbers = (1..=MAX_NUMBER).collect();
+    println!("Sum of squares: {}", sum_of_squares(&numbers));
+    println!("Square of sum: {}", square_of_sum(&numbers));
+
+    println!(
+        "Difference between square of sums and sum of squares of the first {MAX_NUMBER} natural numbers is {}",
+        square_of_sum(&numbers) - sum_of_squares(&numbers)
+    );
+
 }
