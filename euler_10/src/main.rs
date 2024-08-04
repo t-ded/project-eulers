e36592186ec96dc8eaf19870e45fe8dc361f5b7e
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-04 19:52:44 +0200

    assignment 10 done

--- euler_10/src/main.rs
+++ euler_10/src/main.rs
@@ -1,3 +1,29 @@
+const MAX_NUMBER: usize = 2_000_000;
+
+fn sieve_of_eratosthenes(max_num: usize) -> Vec<usize> {
+
+    let mut is_prime = vec![true; max_num + 1];
+    let mut num = 2;
+    while num * num <= max_num {
+        if is_prime[num] {
+            for multiple in ((num * num)..(max_num + 1)).step_by(num) {
+                is_prime[multiple] = false;
+            }
+        }
+        num += 1;
+    }
+
+    let mut primes = Vec::new();
+    for number in 2..(max_num + 1){
+        if is_prime[number] {
+            primes.push(number);
+        }
+    }
+    return primes
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sum: usize = sieve_of_eratosthenes(MAX_NUMBER).into_iter().sum();
+    println!("The sum of all primes below {MAX_NUMBER} is {sum}.");
 }
