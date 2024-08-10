21b49edd0e8483a62f30f3ae5731f3dc21021dc6
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-10 16:21:48 +0200

    assignment 15 done

--- euler_15/src/main.rs
+++ euler_15/src/main.rs
@@ -1,3 +1,13 @@
+fn comb(n: usize, k: usize) -> usize {
+    let k = if k > n - k { n - k } else { k };
+    let mut result = 1;
+    for i in 0..k {
+        result = result * (n - i) / (i + 1);
+    }
+    result
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("The number of possible paths is {}.", comb(40, 20));
 }
