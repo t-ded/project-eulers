638fa6aae7de1e1da144110815d90b1af7df4af5
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-11 21:15:55 +0200

    Assignment 16 finished

--- euler_16/src/main.rs
+++ euler_16/src/main.rs
@@ -1,3 +1,19 @@
+use num_bigint::BigUint;
+use num_traits::One;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mult = BigUint::from(2u8);
+    let mut res = BigUint::one();
+    for _ in 0..1_000 {
+        res = res * &mult;
+    }
+
+    let mut sum = 0;
+    let res_str = res.to_string();
+    for digit in res_str.chars().into_iter() {
+        sum += digit.to_digit(10).unwrap();
+    }
+
+    println!("Sum of digits of 2 ^ 1_000 is {sum}.");
 }
