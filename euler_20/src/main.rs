9f70060b022b093b55629eeb69e56664e53fb4af
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-19 23:15:48 +0200

    Assignment 20 finished

--- euler_20/src/main.rs
+++ euler_20/src/main.rs
@@ -1,3 +1,24 @@
+use num_bigint::BigUint;
+use num_traits::One;
+
+
+fn factorial(n: u128) -> BigUint {
+    let mut fact = BigUint::one();
+    for i in 2..=n {
+        fact *= i;
+    }
+    fact
+}
+
+fn sum_digits(x: impl ToString) -> u128 {
+    let x_str = x.to_string();
+    let mut sum = 0;
+    for digit in x_str.chars() {
+        sum += digit.to_digit(10).unwrap() as u128;
+    }
+    sum
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Sum of digits of 100! is {}.", sum_digits(factorial(99)));
 }
